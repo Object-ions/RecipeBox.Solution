dae806c5d56d33d0dd5c1d708826a60b4b70f8bc
object-ions <moshikoatia@gmail.com>
2023-10-17 13:14:10 -0700

    Update migrations

---
+++ RecipeBox/Migrations/20231017190203_UpdateAuthorModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeBox.Migrations
+{
+    public partial class UpdateAuthorModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RecipeId",
+                table: "Authors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecipeId",
+                table: "Authors");
+        }
+    }
+}
