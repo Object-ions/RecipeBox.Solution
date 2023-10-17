f7919ee00584e54a9cea837c2821fb9337dd6245
object-ions <moshikoatia@gmail.com>
2023-10-17 14:07:54 -0700

    Upade migrations - remove un neccessery filds
    Co-authored-by: Ian Bravo <bravo.ian@gmail.com>

---
+++ RecipeBox/Migrations/20231017205716_UpdateRecipeModelRemoveMesures.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeBox.Migrations
+{
+    public partial class UpdateRecipeModelRemoveMesures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Measure",
+                table: "Recipes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Measure",
+                table: "Recipes",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
